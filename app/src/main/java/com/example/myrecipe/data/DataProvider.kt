d2b80e52f85289d530c09da096e77fba1df3b94f
Kabucho27 <George.Kabucho@strathmore.edu>
2023-06-26 14:14:50 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/myrecipe/data/DataProvider.kt
@@ -0,0 +1,148 @@
+package com.example.myrecipe.data
+
+import com.example.myrecipe.R
+
+object DataProvider {
+
+    val recipeList = listOf(
+        Recipe(
+            id = 2,
+            title = "Omelette",
+            meal = "Breakfast",
+            description = "Here's a simple recipe for preparing a classic omelette:\n" +
+                    "\n" +
+                    "Ingredients:\n" +
+                    "\n" +
+                    "3 large eggs\n" +
+                    "2 tablespoons milk or water\n" +
+                    "Salt and pepper to taste\n" +
+                    "1 tablespoon butter or cooking oil\n" +
+                    "Fillings of your choice (e.g., grated cheese, chopped vegetables, cooked meats, herbs)\n" +
+                    "\n"+
+                    "Instructions:\n" +
+                    "\n" +
+                    "1. Crack the eggs into a bowl and whisk them until the yolks and whites are well combined.\n" +
+                    "\n" +
+                    "2. Add the milk or water to the eggs and whisk again. The addition of milk or water helps create a lighter and fluffier omelette.\n" +
+                    "\n" +
+                    "3. Season the egg mixture with salt and pepper according to your taste. Mix well.\n" +
+                    "\n" +
+                    "4. Heat a non-stick skillet or omelette pan over medium heat. Add the butter or cooking oil and let it melt, ensuring it coats the entire surface of the pan.\n" +
+                    "\n" +
+                    "5. Once the butter has melted and the pan is hot, pour the whisked egg mixture into the pan. Allow it to spread evenly across the surface.\n" +
+                    "\n" +
+                    "6. As the eggs begin to set, gently push the cooked edges toward the center using a spatula. Tilt the pan to allow the uncooked eggs to flow to the edges.\n" +
+                    "\n" +
+                    "7. When the top of the omelette is still slightly runny but mostly set, add your desired fillings on one side of the omelette.\n" +
+                    "\n" +
+                    "8. Using the spatula, carefully fold the empty side of the omelette over the side with the fillings, creating a half-moon shape.\n" +
+                    "\n" +
+                    "9. Cook for another minute or so to ensure the fillings are heated through and any cheese has melted.\n" +
+                    "\n" +
+                    "10. Slide the omelette onto a plate, folding it in half if necessary, and serve immediately.\n" +
+                    "\n" +
+                    "This recipe will serve 1 person. You can garnish the omelette with fresh herbs, such as chopped parsley or chives, if desired. Serve it as is or with toast, salad, or any sides of your choice. Feel free to experiment with different fillings to create your own delicious omelette variations.",
+            recipeImageId = R.drawable.omlette
+        ),
+        Recipe(
+            id = 2,
+            title = "Rice Beef Stew",
+            meal = "Lunch",
+            description = "Learn how to prepare yout favorite rice and beef stew:\n" +
+                    "Here's a recipe for preparing rice and beef stew:\n" +
+                    "\n" +
+                    "Ingredients:\n" +
+                    "\n" +
+                    "1 lb (450g) beef stew meat, cubed\n" +
+                    "1 onion, chopped\n" +
+                    "2 cloves garlic, minced\n" +
+                    "2 carrots, peeled and chopped\n" +
+                    "2 celery stalks, chopped\n" +
+                    "2 tablespoons vegetable oil\n" +
+                    "2 tablespoons all-purpose flour\n" +
+                    "2 cups beef broth\n" +
+                    "1 cup diced tomatoes\n" +
+                    "1 teaspoon dried thyme\n" +
+                    "1 teaspoon paprika\n" +
+                    "Salt and pepper to taste\n" +
+                    "2 cups cooked rice\n" +
+                    "\n"+
+                    "Instructions:\n" +
+                    "\n" +
+                    "1. Heat the vegetable oil in a large pot or Dutch oven over medium-high heat.\n" +
+                    "\n" +
+                    "2. Add the beef stew meat to the pot and cook until browned on all sides. Remove the meat from the pot and set it aside.\n" +
+                    "\n" +
+                    "3. In the same pot, add the chopped onion, minced garlic, carrots, and celery. Sauté until the vegetables are slightly softened, about 5 minutes.\n" +
+                    "\n" +
+                    "4. Sprinkle the flour over the vegetables and stir well to coat everything. Cook for an additional minute to cook off the raw flour taste.\n" +
+                    "\n" +
+                    "5. Slowly pour in the beef broth while stirring constantly. This will help to create a smooth sauce.\n" +
+                    "\n" +
+                    "6. Add the diced tomatoes, dried thyme, paprika, salt, and pepper to the pot. Stir everything together.\n" +
+                    "\n" +
+                    "7. Return the browned beef stew meat to the pot. Stir well to combine with the vegetables and sauce.\n" +
+                    "\n" +
+                    "8. Bring the stew to a boil, then reduce the heat to low. Cover the pot and simmer for about 1.5 to 2 hours, or until the beef is tender and the flavors have melded together.\n" +
+                    "\n" +
+                    "9. While the stew is simmering, prepare the rice according to the package instructions or your preferred method.\n" +
+                    "\n" +
+                    "10. Once the beef stew is cooked and the flavors have developed, taste and adjust the seasoning if needed.\n" +
+                    "\n" +
+                    "11. Serve the beef stew over a bed of cooked rice. Enjoy!\n" +
+                    "\n" +
+                    "This recipe will make approximately 4 servings of rice and beef stew. Feel free to adjust the ingredients and quantities based on your preferences and the number of people you are serving.",
+            recipeImageId = R.drawable.riceandbeefstew
+        ),
+        Recipe(
+            id = 3,
+            title = "Spaghetti Bolognese",
+            meal = "Supper",
+            description = "Here's a recipe for preparing spaghetti with minced meat (also known as spaghetti bolognese):\n" +
+                    "\n" +
+                    "Ingredients:\n" +
+                    "\n" +
+                    "8 ounces (225g) spaghetti\n" +
+                    "1 lb (450g) ground beef or minced meat\n" +
+                    "1 onion, chopped\n" +
+                    "2 cloves garlic, minced\n" +
+                    "1 carrot, grated\n" +
+                    "1 celery stalk, chopped\n" +
+                    "1 can (14 oz or 400g) diced tomatoes\n" +
+                    "2 tablespoons tomato paste\n" +
+                    "1 teaspoon dried oregano\n" +
+                    "1 teaspoon dried basil\n" +
+                    "1/2 teaspoon sugar\n" +
+                    "Salt and pepper to taste\n" +
+                    "Grated Parmesan cheese (optional)\n" +
+                    "Fresh basil or parsley for garnish (optional)\n" +
+                    "\n" +
+                    "Instructions:\n" +
+                    "\n" +
+                    "1. Cook the spaghetti according to the package instructions until al dente. Drain and set aside.\n" +
+                    "\n" +
+                    "2. In a large skillet or saucepan, heat some olive oil over medium heat.\n" +
+                    "\n" +
+                    "3. Add the chopped onion and minced garlic to the pan. Sauté until the onion becomes translucent and the garlic is fragrant, about 2-3 minutes.\n" +
+                    "\n" +
+                    "4. Add the ground beef or minced meat to the pan. Break it up with a spatula or wooden spoon and cook until it's browned and cooked through.\n" +
+                    "\n" +
+                    "5. Add the grated carrot and chopped celery to the pan. Sauté for a few minutes until the vegetables start to soften.\n" +
+                    "\n" +
+                    "6. Stir in the diced tomatoes, tomato paste, dried oregano, dried basil, sugar, salt, and pepper. Mix well to combine all the ingredients.\n" +
+                    "\n" +
+                    "7. Reduce the heat to low and let the sauce simmer for about 15-20 minutes, stirring occasionally to ensure the flavors meld together.\n" +
+                    "\n" +
+                    "8. While the sauce is simmering, you can taste and adjust the seasoning if needed.\n" +
+                    "\n" +
+                    "9. Once the sauce has thickened and the flavors have developed, it's time to serve. Place a portion of cooked spaghetti on a plate or bowl and top it with a generous amount of the minced meat sauce.\n" +
+                    "\n" +
+                    "10. If desired, sprinkle grated Parmesan cheese over the top of each serving. You can also garnish with fresh basil or parsley for added freshness.\n" +
+                    "\n" +
+                    "11. Serve the spaghetti and minced meat hot and enjoy!\n" +
+                    "\n" +
+                    "This recipe will make approximately 4 servings of spaghetti with minced meat. Feel free to adjust the quantities of the ingredients based on your preferences and the number of people you are serving.",
+            recipeImageId = R.drawable.spaghettibolognese
+        )
+    )
+}
---
+++ app/src/main/java/com/example/myrecipe/data/Recipe.kt
@@ -0,0 +1,11 @@
+package com.example.myrecipe.data
+
+import java.io.Serializable
+
+data class Recipe (
+    val id: Int,
+    val title: String,
+    val meal: String,
+    val description: String,
+    val recipeImageId: Int = 0
+) : Serializable
